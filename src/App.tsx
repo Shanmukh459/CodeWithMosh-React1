222fbde7ae563673b6d24520fe8a8e4fa022d9f6
Shanmukh459 <saikrrish459@gmail.com>
2024-07-01 13:45:13 -0700

    passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import ListGroup from "./components/ListGroup"
+import Alert from "./components/Alert"
 
 function App() {
-  const items = ["New York", "San Francisco", "London", "Tokyo", "Paris"]
-  const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
+  // const items = ["New York", "San Francisco", "London", "Tokyo", "Paris"]
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item)
+  // }
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World!</span>
+      </Alert>
     </div>
   )
 }
