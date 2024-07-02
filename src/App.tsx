2d61e0e4c26866af3db4db37ea564361fa2acc98
Shanmukh459 <saikrrish459@gmail.com>
2024-07-02 12:15:03 -0700

    form creation and submitting without default behaviour

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { useState } from "react"
-import ExpandableText from "./components/ExpandableText"
+import Form from "./components/Form"
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={20}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit qui
-        sequi tenetur ratione impedit natus quod necessitatibus laboriosam
-        aspernatur nam alias vitae autem officiis officia, aliquam sed sit ea
-        ducimus molestiae, vel, iusto sapiente? Neque mollitia dolorem ea iusto.
-        Aperiam incidunt aut necessitatibus accusantium illum reprehenderit
-        perspiciatis temporibus labore mollitia pariatur autem, maiores rem?
-        Alias, sit nemo. Sunt blanditiis harum autem aspernatur tempora
-        obcaecati magnam error ab ut, ex iusto exercitationem sint aliquam
-        corporis nisi quae commodi vel dolorem repudiandae? Facilis cum sint
-        necessitatibus vel inventore sapiente tenetur blanditiis nihil debitis
-        expedita, veniam, aliquam sequi architecto iste deleniti. Sint, quasi.
-      </ExpandableText>
+      <Form />
     </div>
   )
 }
