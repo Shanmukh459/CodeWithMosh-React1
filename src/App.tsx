429c065556228dbcfa3faefce9e09261aec4ad8b
Shanmukh459 <saikrrish459@gmail.com>
2024-07-01 22:09:43 -0700

    updating state of an nested object9increasing the quatity by 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import { useState } from "react"
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppins: ["Mushrooms"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product1", quantity: 1 },
+      { id: 2, title: "Product2", quantity: 1 },
+    ],
   })
 
   const handleChange = () => {
-    setPizza({
-      ...pizza,
-      toppins: [...pizza.toppins, "Cheese"],
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: 2 } : item
+      ),
     })
   }
   return (
     <div>
-      <p>Name: {pizza.name}</p>
       <ul>
-        {pizza.toppins.map((toppin) => (
-          <li key={toppin}>{toppin}</li>
+        {cart.items.map((item) => (
+          <p key={item.id}>
+            {item.title}: {item.quantity}
+          </p>
         ))}
       </ul>
       <button onClick={handleChange}>Change</button>
