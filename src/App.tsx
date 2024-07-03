1d5054fb3073cc953702caa59b29bfe07cab8626
Shanmukh459 <saikrrish459@gmail.com>
2024-07-03 12:35:23 -0700

    updateting user using patch

--- src/App.tsx
+++ src/App.tsx
@@ -58,6 +58,23 @@ function App() {
       })
   }
 
+  const updateUser = (user: User) => {
+    const originalUsers = [...users]
+
+    const updatedUser = { ...user, name: user.name + "!" }
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)))
+
+    axios
+      .patch(
+        "https://jsonplaceholder.typicode.com/users/" + user.id,
+        updatedUser
+      )
+      .catch((err) => {
+        setError(err.message)
+        setUsers(originalUsers)
+      })
+  }
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
@@ -72,13 +89,21 @@ function App() {
               key={user.id}
               className="list-group-item d-flex justify-content-between"
             >
-              {user.name}{" "}
-              <button
-                className="btn btn-outline-danger"
-                onClick={() => deleteUser(user)}
-              >
-                Delete
-              </button>
+              {user.name}
+              <div>
+                <button
+                  className="btn btn-outline-secondary mx-2"
+                  onClick={() => updateUser(user)}
+                >
+                  Update
+                </button>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => deleteUser(user)}
+                >
+                  Delete
+                </button>
+              </div>
             </li>
           ))}
         </ul>
